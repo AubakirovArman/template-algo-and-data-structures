52ae832a6ab5b4a91810f94972dd896a7310a007
sakenism <saken.mukanov@nu.edu.kz>
2020-07-10 15:35:21 +0600

    first commit

--- loops-for/arrayLoop.py
+++
@@ -1,2 +0,0 @@
-def insert_squares(arr, num):
-    # add square of numbers from 1 to num to the list named arr and return list
--- loops-for/forDict.py
+++
@@ -1,2 +0,0 @@
-def reverse_dict(dict):
-    # swap keys and values within dict and return dict
